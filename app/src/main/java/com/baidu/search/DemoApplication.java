613d6d7ea9010c7b65e8d8d5041a86d08c4f487d
zhuhao211554 <307167682@qq.com>
2017-07-17 11:37:10 +0800

    增加效果图

--- app/src/main/java/com/baidu/search/DemoApplication.java
+++ app/src/main/java/com/baidu/search/DemoApplication.java
@@ -15,6 +15,7 @@ public class DemoApplication extends Application {
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
         SDKInitializer.setCoordType(CoordType.BD09LL);
+        System.out.println();
     }
 
 }
